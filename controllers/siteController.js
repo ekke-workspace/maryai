2ebbca0b0138234acade602139ffa97e37b616c4
Nusetor Foster <nusetorfoster77@gmail.com>
2023-08-14 08:04:54 +0000

    Added user interface

--- controllers/siteController.js
+++ controllers/siteController.js
@@ -16,8 +16,8 @@ app.get('/home', function(req, res){
 
 // Handling non matching request from the client
 app.use((req, res, next) => {
-    res.status(404).send(
-        "<h1>404 Page not found on the server</h1>")
+    res.status(404).render(
+        '404')
 })
 
 }
